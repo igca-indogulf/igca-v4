ccfcf0bcb97a4a7780a6a6ad9e963e8e15bb17a7
igca-indogulf <manas@modernteams.ai>
2026-09-11 19:49:32 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/igca-v4/',
   plugins: [react()],
   resolve: {
     alias: {
